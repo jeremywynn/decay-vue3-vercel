9cb72af8ecfc1f656674d063350aa9bfe30ea5c9
Jeremy Wynn <jeremy@jeremywynn.com>
2020-10-20 14:58:28 -0500

    First 2 working Serverless Functions!

---
+++ api/get-categories.js
@@ -0,0 +1,19 @@
+const { Stitch, UserApiKeyCredential } = require('mongodb-stitch-server-sdk')
+
+let client
+if (Stitch.hasAppClient(process.env.MONGODB_STITCH_APP_ID)) {
+  client = Stitch.getAppClient(process.env.MONGODB_STITCH_APP_ID)
+} else {
+  client = Stitch.initializeDefaultAppClient(process.env.MONGODB_STITCH_APP_ID)
+}
+const credential = new UserApiKeyCredential(process.env.MONGODB_API_KEY)
+module.exports = async(req, res) => {
+  try {
+    await client.auth.loginWithCredential(credential)
+    const categories = await client.callFunction('getCategories')
+    res.json(categories)
+  } catch (error) {
+    res.status(500)
+    res.send(error)
+  }
+}
